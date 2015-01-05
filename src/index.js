ad0d19c5b85765d7da0ee3799213ceb96fd23836
Kier Borromeo <seraphipod@gmail.com>
2015-01-05 22:09:20 +0800

    Added mixins to exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 exports.ContainerMixin = require('./ContainerMixin');
+
 exports.Drawer = require('./Drawer');
+exports.DrawerMixin = require('./DrawerMixin');
+
 exports.Message = require('./Message');
+exports.MessageMixin = require('./MessageMixin');
