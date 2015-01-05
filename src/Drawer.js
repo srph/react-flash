7eee6874b6eca48c7d83a78806d16898055e9738
Kier Borromeo <seraphipod@gmail.com>
2015-01-05 22:50:54 +0800

    Messages are now filtered
    Added styling attributes
    Added proper PropType to each propType.
    Renamed propType `message` to `data`; `remove` to `removeHandler`

--- src/Drawer.js
+++ src/Drawer.js
@@ -5,34 +5,57 @@ var Message = require('./Message');
 /**
  * Default drawer style
  */
-var DrawerStyle = {
+var DefaultStyle = {
   'position': 'absolute',
-  'right': '25',
-  'top': '25'
+  'left': 'auto',
+  'right': 'auto',
+  'top': '25px'
 }; 
 
 /**
  * More like a category of messages,
- * or a container of certain messages
+ * or a container of messages of certain type
  */
 var Drawer = React.createClass({
   propTypes: {
+    // Type of message to be filtered, filter == message.type
+    filter: React.PropTypes.string,
+    // Styling attributes, attributes: { class: .., style: .. };
+    attributes: React.PropTypes.object,
+    // Messages
     stack: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-    filter: React.PropTypes.string
+    
+    // Remove handler passed from the Container
+    // down to the Messages
+    removeHandler: React.PropTypes.function.isRequired,
   },
-  getDefaultProps: function() {
-    return { stack: [] };
-  },
+  
+  getDefaultProps: function() { return { attributes: {} }; },
+  
   render: function() {
+    // Props shorthand
+    var filter = this.props.filter;
     var stack = this.props.stack;
-    var remove = this.props.remove;
+    var removeHandler = this.props.removeHandler;
+    
+    // Attributes
+    var attributes = this.props.attributes;
+    var style = attributes.style || DefaultStyle;
+    var className = attributes.className || '';
+    
+    // Message template
+    var template = stack
+      .map(function(message, index) {
+        return <Message
+          data={message}
+          key={index}
+          removeHandler={removeHandler} />
+      });
 
     return (
-      <div style={DrawerStyle}>
-        {stack.map(function(message, index) {
-          return (
-            <Message message={message} key={message.id} remove={remove} />
-          );
+      <div style={style} className={className}>
+        {template.filter(function(message, index) {
+          return filter ? message.type == filter : true;
         })}
       </div>
     );
